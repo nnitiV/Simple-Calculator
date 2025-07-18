bfdfb0247d3508a98616a89c2e523b90da936e5b
nnitivv <vihstudent@gmail.com>
2025-07-18 14:54:36 -0300

    Better calculator made

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,103 +1,86 @@
 import { useState } from "react";
+import History from "./history";
 import styles from "./index.module.css"
-import History from "./History";
 
 const Calculator = () => {
-    const [screenText, setScreenText] = useState<string>("");
+    const [screenText, setScreenText] = useState<string>("0");
     const [calculus, setCalculus] = useState<string>("");
-    const [wasCalculusEvaluted, setWasCalculusEvaluated] = useState<boolean>(false);
-    const [wasLastNumber, setWasLastNumber] = useState<boolean>(false)
-    const [calculusHistory, setCalculusHistory] = useState<string[]>([]);
-    const [lastRetrievedHistoryIndex, setLastRetrievedHistoryIndex] = useState<number>(-1);
-
-    const symbolClick = (symbol: string) => {
-        setWasLastNumber(false)
-        setScreenText(symbol)
-        // Fixed the calculus string if you already evaluated the calculus
-        if (wasCalculusEvaluted) {
-            setWasCalculusEvaluated(false)
-            return setCalculus(screenText + symbol);
-        }
-        // 1+ => 1+++
-        if (!wasLastNumber) {
-            setCalculus(prev => prev.slice(0, -1))
-            return setCalculus(prev => prev + symbol);
-        }
-        setCalculus(prev => prev + screenText + symbol);
-    }
+    const [resultEvaluted, setResultEvaluated] = useState<boolean>(false);
+    const [calcHistory, setCalcHistory] = useState<string>("");
+    const [history, setHistory] = useState<string[]>([]);
 
     const numberClick = (number: string) => {
-        setWasLastNumber(true)
-        // If you clicked a symbol or just started the app, resets the screen text
-        if (!parseInt(screenText)) {
-            setWasCalculusEvaluated(false)
-            return setScreenText(number)
+        if (screenText == "0" || !Number(screenText)) {
+            return setScreenText(number);
         }
-        // If you evaluted the calculus and wanted it to start a new one just by clicking in a number, it resets it
-        if (wasCalculusEvaluted) {
-            setCalculus("")
-            setWasCalculusEvaluated(false)
-            return setScreenText(number)
-        }
-        // Adds another number to the number on the screen text
         setScreenText(prev => prev + number)
     }
 
-    const clear = () => {
-        setScreenText("")
-        setCalculus("")
+    const symbolClick = (symbol: string) => {
+        setScreenText(symbol == "*" ? "X" : symbol);
+        if (Number(screenText)) {
+            if (resultEvaluted) {
+                setCalculus(prev => prev + symbol);
+                setResultEvaluated(false);
+            } else {
+                setCalculus(prev => prev + screenText + symbol);
+            }
+        } else {
+            setCalculus(prev => prev.slice(0, -1) + symbol);
+        }
     }
 
     const evalCalculus = () => {
-        if (!wasLastNumber) {
-            return alert("You need to select a number before evaluating!")
-        }
-        const calculusEvaluated = String(eval((calculus + screenText)))
-        if (lastRetrievedHistoryIndex !== -1) setCalculusHistory(prev => [...prev, calculusHistory[lastRetrievedHistoryIndex]])
-        setCalculusHistory(prev => [...prev, calculus + screenText])
-        setLastRetrievedHistoryIndex(-1)
-        setCalculus(calculusEvaluated)
-        setScreenText(calculusEvaluated)
-        setWasCalculusEvaluated(true)
+        const result = String(eval(calculus + screenText));
+        setScreenText(result);
+        setCalculus(result);
+        setResultEvaluated(true);
+        if (calcHistory) setHistory(prev => [...prev, calcHistory])
+        setHistory(prev => [...prev, calculus + screenText])
     }
 
-    const goBackToEvaluatedCalculus = (index: number) => {
-        const evalCalculus = String(eval(calculusHistory[index]))
-        setScreenText(evalCalculus);
-        setCalculus(evalCalculus);
-        setLastRetrievedHistoryIndex(index);
+    const clear = () => {
+        setCalculus("");
+        setScreenText("");
+        setResultEvaluated(false);
     }
 
-    const clearHistory = () => setCalculusHistory([]);
+    const clearHistory = () => setHistory([]);
+
+    const goToCalculusHistory = (calc: string) => {
+        const evalCalc = String(eval(calc));
+        setCalcHistory(calc);
+        setScreenText(evalCalc);
+        setCalculus(evalCalc);
+        setResultEvaluated(true);
+    }
 
     return (
-        <>
-            <section className={styles.calculator}>
-                <input className={styles.screen} value={screenText ? screenText : 0} />
-                <div className={styles.buttonsHeader}>
-                    <button onClick={clear}>Clear</button>
-                    <button onClick={() => setScreenText(prev => prev.slice(0, -1))}><img src="https://img.icons8.com/?size=100&id=87017&format=png&color=000000s" alt="" /></button>
-                </div>
+        <section>
+            <div className={styles.calculator}>
+                <div className={styles.visor}>{screenText == "0" || !screenText ? "0" : screenText}</div>
                 <div className={styles.buttons}>
+                    <button className={styles.twoColumns} onClick={clear}>Clear</button>
+                    <button className={styles.twoColumns2} onClick={() => setScreenText(prev => prev.slice(0, -1))}>Erase</button>
                     <button onClick={() => numberClick("7")}>7</button>
                     <button onClick={() => numberClick("8")}>8</button>
                     <button onClick={() => numberClick("9")}>9</button>
-                    <button onClick={() => symbolClick("/")}>/</button>
+                    <button className={styles.orange} onClick={() => symbolClick("*")}>X</button>
                     <button onClick={() => numberClick("4")}>4</button>
                     <button onClick={() => numberClick("5")}>5</button>
                     <button onClick={() => numberClick("6")}>6</button>
-                    <button onClick={() => symbolClick("*")}>X</button>
+                    <button className={styles.orange} onClick={() => symbolClick("/")}>/</button>
                     <button onClick={() => numberClick("1")}>1</button>
                     <button onClick={() => numberClick("2")}>2</button>
                     <button onClick={() => numberClick("3")}>3</button>
-                    <button onClick={() => symbolClick("-")}>-</button>
-                    <button onClick={() => numberClick("0")} className={styles.spanRow}>0</button>
-                    <button onClick={() => evalCalculus()}>=</button>
-                    <button onClick={() => symbolClick("+")}>+</button>
+                    <button className={styles.orange} onClick={() => symbolClick("-")}>-</button>
+                    <button className={styles.zero} onClick={() => numberClick("0")}>0</button>
+                    <button onClick={evalCalculus}>=</button>
+                    <button className={styles.orange} onClick={() => symbolClick("+")}>+</button>
                 </div>
-            </section >
-            <History history={calculusHistory} goBackToEvaluatedCalculus={goBackToEvaluatedCalculus} clearHistory={clearHistory} />
-        </>
+            </div>
+            <History history={history} clearHistory={clearHistory} goToCalculusHistory={goToCalculusHistory} />
+        </section>
     )
 };
 
